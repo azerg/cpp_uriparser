3f351139d6cbc541b345e32de0830d87463a9427
azerg <pub@azerg.com>
2015-11-07 21:55:46 +0200

    scripts fix

--- cpp_uriparser/cmake_build.sh
+++ cpp_uriparser/cmake_build.sh
@@ -2,12 +2,12 @@ TARGTET_DIR=$(readlink -f .)
 echo '*****************************************************'
 BOOST_ROOT="$TARGTET_DIR/boost_1_46_1"
 GTEST_DIR="$TARGTET_DIR/gtest-1.7.0"
-echo $BOOST_DIR
+echo $BOOST_ROOT
 echo $TARGTET_DIR
 
 echo '*****************************************************'
 
-cmake . -B"build" -Wno-dev -DBOOST_ROOT="$BOOST_DIR" -DGTEST_FOLDER="$GTEST_DIR" -DGTEST_LIBRARY="$GTEST_DIR"/cmake/libgtest.so
+cmake . -B"build" -Wno-dev -DBOOST_ROOT="$BOOST_ROOT" -DGTEST_FOLDER="$GTEST_DIR" -DGTEST_LIBRARY="$GTEST_DIR"/cmake/libgtest.so
 #cd build
 #make
 #cd ..
